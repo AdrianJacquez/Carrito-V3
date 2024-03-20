c0e98fb30a500a2f570fb6faba0bd4210df12a60
MINGW64 <Adrijesuslerma@outlook.com>
2024-03-20 13:03:06 -0700

    CarritoV3-finish

--- script.js
+++ script.js
@@ -1,7 +1,97 @@
-//Uso de e.stopPropagation() para evitar el burbujeo de los elementos
-const formulario = document.querySelector("form");
+const carrito = document.getElementById("carrito");
+const template = document.getElementById("template");
+const footer = document.getElementById("footer");
+const templateFooter = document.getElementById("templateFooter");
+const fragment = document.createDocumentFragment();
 
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("Me diste click ");
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".card .btn-primary")) {
+    console.log("ejecutar agregar al carrito");
+    agregarCarrito(e);
+  }
+  if (e.target.matches(".list-group-item .btn-success")) {
+    btnAumentar(e);
+  }
+  if (e.target.matches(".list-group-item .btn-danger")) {
+    btnDisminuir(e);
+  }
 });
+
+let carritoObjeto = [];
+
+const agregarCarrito = (e) => {
+  // console.log(e.target.dataset);
+  // console.log(e.target.dataset.fruta);
+
+  const producto = {
+    titulo: e.target.dataset.fruta,
+    id: e.target.dataset.fruta,
+    cantidad: 1,
+    precio: parseInt(e.target.dataset.precio),
+  };
+
+  const indice = carritoObjeto.findIndex((item) => item.id === producto.id);
+  if (indice === -1) {
+    carritoObjeto.push(producto);
+  } else {
+    carritoObjeto[indice].cantidad++;
+  }
+  console.log(carritoObjeto);
+
+  pintarCarrito();
+  pintarFooter();
+};
+
+const pintarCarrito = () => {
+  carrito.textContent = "";
+
+  Object.values(carritoObjeto).forEach((item) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".lead").textContent = item.titulo;
+    clone.querySelector(".rounded-pill").textContent = item.cantidad;
+    clone.querySelector(".badge").textContent = item.cantidad;
+    clone.querySelector("div .lead span").textContent =
+      item.precio * item.cantidad;
+    clone.querySelector(".btn-danger").dataset.id = item.id;
+    clone.querySelector(".btn-success").dataset.id = item.id;
+    fragment.appendChild(clone);
+  });
+  carrito.appendChild(fragment);
+  pintarFooter();
+};
+
+const pintarFooter = () => {
+  console.log("pintar footer");
+  footer.textContent = "";
+  const total = carritoObjeto.reduce(
+    (acc, current) => acc + current.cantidad * current.precio,
+    0
+  );
+  const clone = templateFooter.content.cloneNode(true);
+  clone.querySelector("span").textContent = total;
+  footer.appendChild(clone);
+};
+
+const btnAumentar = (e) => {
+  console.log("me diste click", e.target.dataset.id);
+  carritoObjeto = carritoObjeto.map((item) => {
+    if (item.id === e.target.dataset.id) {
+      item.cantidad++;
+    }
+    return item;
+  });
+  pintarCarrito();
+};
+
+const btnDisminuir = (e) => {
+  console.log("me diste click", e.target.dataset.id);
+  carritoObjeto = carritoObjeto.map((item) => {
+    if (item.id === e.target.dataset.id) {
+      if (item.cantidad > 0) {
+        item.cantidad--;
+      }
+    }
+    return item;
+  });
+  pintarCarrito();
+};
