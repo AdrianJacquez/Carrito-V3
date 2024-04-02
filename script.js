f1acc74ad927b6efb59bfa54984227e991a5fa64
MINGW64 <Adrijesuslerma@outlook.com>
2024-04-02 11:02:58 -0700

    mejora al carrito

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const carrito = document.getElementById("carrito");
 const template = document.getElementById("template");
-const footer = document.getElementById("footer");
+const Footer = document.getElementById("Footer");
 const templateFooter = document.getElementById("templateFooter");
 const fragment = document.createDocumentFragment();
+const finalizarCompra = document.getElementById("finalizarCompra");
 
+//evento de click que abarca a varios botones
 document.addEventListener("click", (e) => {
   if (e.target.matches(".card .btn-primary")) {
     console.log("ejecutar agregar al carrito");
     agregarCarrito(e);
   }
   if (e.target.matches(".list-group-item .btn-success")) {
+    console.log("ejecutar agregar al carrito");
     btnAumentar(e);
   }
   if (e.target.matches(".list-group-item .btn-danger")) {
+    console.log("ejecutar agregar al carrito");
     btnDisminuir(e);
+  }
+  if (e.target.matches(".card .btn-warning")) {
+    console.log("Compra finalizada");
+    btnFinalizarCompra();
   }
 });
 
@@ -49,13 +57,35 @@ const pintarCarrito = () => {
     clone.querySelector(".lead").textContent = item.titulo;
     clone.querySelector(".rounded-pill").textContent = item.cantidad;
     clone.querySelector(".badge").textContent = item.cantidad;
-    clone.querySelector("div .lead span").textContent =
-      item.precio * item.cantidad;
+    clone.querySelector("div .lead span").textContent = formatCurrency(
+      item.precio * item.cantidad
+    );
+
     clone.querySelector(".btn-danger").dataset.id = item.id;
     clone.querySelector(".btn-success").dataset.id = item.id;
     fragment.appendChild(clone);
   });
   carrito.appendChild(fragment);
+  pintarFooter();
+};
+
+const formatCurrency = (amount) => {
+  // Formatear el número como moneda
+  return amount.toLocaleString("es-ES", { style: "currency", currency: "USD" });
+};
+
+const pintarFooter = () => {
+  Footer.textContent = "";
+
+  const total = carritoObjeto.reduce(
+    (acc, current) => acc + current.precio * current.cantidad,
+    0
+  );
+
+  const clone = templateFooter.content.cloneNode(true);
+  clone.querySelector("p span").textContent = formatCurrency(total);
+
+  Footer.appendChild(clone);
 };
 
 const btnAumentar = (e) => {
@@ -77,6 +107,22 @@ const btnDisminuir = (e) => {
         item.cantidad--;
       }
     }
+    return item;
   });
   pintarCarrito();
+};
+
+//muestra un mensaje despues de que se le diera click a un boton, y al darle aceptar la pagina se restaura
+const btnFinalizarCompra = () => {
+  Swal.fire({
+    title: "Compra realizada con éxito",
+    text: "¡Gracias, vuelva pronto!",
+    icon: "success",
+  }).then((result) => {
+    // Si el usuario confirma la alerta
+    if (result.isConfirmed) {
+      //recarga la pagina despues de la confirmacion del la alerta
+      window.location.reload();
+    }
+  });
 };
